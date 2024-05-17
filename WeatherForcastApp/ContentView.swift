a8252862f83f27ca8c9f44e3b436ac0e4ad2cf97
Rohith <kunarohith@gmail.com>
2024-05-17 11:04:37 +0530

    Initial Commit

---
+++ WeatherForcastApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherForcastApp
+//
+//  Created by Kune Rohith on 27/02/1946 Saka.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherForcastApp/WeatherForcastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForcastAppApp.swift
+//  WeatherForcastApp
+//
+//  Created by Kune Rohith on 27/02/1946 Saka.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForcastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
